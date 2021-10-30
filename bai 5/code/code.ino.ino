ff279a22242e6fb17972cc42cf0e44a39d1517f6
phongctt60cntt <63570313+phongctt60cntt@users.noreply.github.com>
2021-10-30 08:55:20 +0700

    update 1

--- bai 5/code/code.ino.ino
+++ bai 5/code/code.ino.ino
@@ -1,10 +1,6 @@
 #include <LiquidCrystal.h>
-
 int THERMISTORPIN = 0,  BCOEFFICIENT = 3380 ;
-
 float THERMISTORNOMINAL = 10000 , TEMPERATURENOMINAL = 25 , SERIESRESISTOR = 10000    ;
-
-
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -15,22 +11,15 @@ void setup() {
  lcd.begin(16, 2);
 
 }
-   
-
 void loop() {
-
-
-  
   int i;
   float average;
  
-  // take N samples in a row, with a slight delay
+  // lấy N mẫu liên tiếp
   for (i=0; i< 5; i++) {
    sample[i] = analogRead(THERMISTORPIN);
    delay(10);
   }
-
-  // average all the samples out
   average = 0;
   
   for (i=0; i< 5; i++) {
@@ -38,18 +27,18 @@ void loop() {
      
   }
   average /= 5;
-   // convert the value to resistance
+   // chuyển đổi giá trị qua điện trở
   average = 1023 / average - 1;
   average = SERIESRESISTOR / average;
  
  
   float steinhart;
-  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart -= 273.15;                         // convert to C
+  steinhart = average / THERMISTORNOMINAL;     
+  steinhart = log(steinhart);                  
+  steinhart /= BCOEFFICIENT;                   
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); 
+  steinhart = 1.0 / steinhart;                 
+  steinhart -= 273.15;                         // chuyển đổi sang độ C
 
   lcd.print("Temp  = ");
   lcd.print((int)steinhart);   
